2ac9b792379312d2e7a25345370abcd3c82e5b11
rodrigopulse <contato@rodrigogodoy.net>
2019-10-31 21:36:15 -0300

    hero versão desktop

--- src/componentes/Hero/Hero.js
+++ src/componentes/Hero/Hero.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Thumb from '../../assets/img/hero/hero-home-desktop.jpg';
 
-import './Hero.css';
+import '../../assets/sass/botoes.scss'
+import './Hero.scss';
+import Curvas from './HeroCurvas.svg';
 
 class Hero extends React.Component {
     constructor() {
@@ -9,12 +11,19 @@ class Hero extends React.Component {
         const estiloThumb = {
             backgroundImage: 'url(' + Thumb + ')'
         }
-        this.state={estiloThumb};
+        this.state={ estiloThumb };
     }
     render(){
         return(
-            <div className="hero">
-                <div className="hero__thumb" style={ this.state.estiloThumb }></div>
+            <div className="hero"  style={ this.state.estiloThumb }>
+                <div className="hero__conteudo">
+                    <h1 className="hero__titulo">Telefonia em nuvem</h1>
+                    <h2 className="hero__subtitulo">Plataforma de Comunicação Panasonic em nuvem com serviço personalizado.</h2>
+                    <a className="botao-padrao" href="#">Entrar em Contato</a>
+                </div>
+                <div className="hero__curvas">
+                    <img src={Curvas} alt="Curvas" />
+                </div>
             </div>
         );
     }
