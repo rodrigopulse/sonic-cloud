487eb2404cf6a2b8c51c6846589050badbdd514a
rodrigopulse <contato@rodrigogodoy.net>
2019-11-10 13:41:29 -0300

    feita a página de contato

--- src/layout/Footer/Footer.js
+++ src/layout/Footer/Footer.js
@@ -3,22 +3,22 @@ import FormContato from '../../componentes/FormContato/FormContato';
 import './Footer.scss';
 
 class Footer extends React.Component {
-    render() {
-        return(
-            <footer className="footer">
+	render() {
+		return(
+			<footer className="footer">
 
-                <h3 className="footer__titulo">
-                    Contato
-                </h3>
-                <p>
-                    Entre em contato conosco, teremos o maior prazer em te ajudar a gastar menos e ter maior eficiência na comunicação da sua empresa.
-                </p>
+				<h3 className="footer__titulo">
+					Contato
+				</h3>
+				<p>
+					Entre em contato conosco, teremos o maior prazer em te ajudar a gastar menos e ter maior eficiência na comunicação da sua empresa.
+				</p>
 
-                <FormContato />
+				<FormContato />
 
-            </footer>
-        );
-    }
+			</footer>
+		);
+	}
 
 }
 export default Footer;
--- src/paginas/Contato/Contato.js
+++ src/paginas/Contato/Contato.js
@@ -1,12 +1,56 @@
 import React from 'react';
 
+//Layout
+import Header from '../../layout/Header/Header';
+import FormContato from '../../componentes/FormContato/FormContato';
+import Hero from '../../componentes/Hero/Hero';
+//Imagens
+import imagemHero from '../../assets/img/hero/hero-contato-desktop.jpg';
+
+import './Contato.scss';
+
 class Contato extends React.Component {
-    render() {
-        return(
-            <div className='container container-contato'>
-            </div>
-        );
-    }
+	render() {
+		return(
+			<div className='container'>
+				<Header
+					paginaAtual = 'contato'
+				/>
+				<Hero
+					titulo = "Entre em Contato"
+					subtitulo = "Teremos um imenso prazer em te atender."
+					imagem = {imagemHero}
+				/>
+				<div className="contato container--max">
+					<div className="contato__conteudo">
+						<div className="contato__conteudo-item">
+							<p className="contato__texto">
+								<span>Endereço:</span>
+								Rua Ribeiro Lacerda, 283 <br />
+								Jd. Da Saúde - São Paulo - SP
+							</p>
+							<a className="botao-padrao" href="/contato">Ver no mapa</a>
+						</div>
+						<div className="contato__conteudo-item">
+							<p className="contato__texto">
+								<span>Telefone:</span>
+								11 2463-7890
+							</p>
+						</div>
+						<div className="contato__conteudo-item">
+							<p className="contato__texto">
+								<span>Whats App:</span>
+								11 99898-7998
+							</p>
+						</div>
+					</div>
+					<div className="contato__formulario">
+						<FormContato />
+					</div>
+				</div>
+			</div>
+		);
+	}
 
 }
 export default Contato;
