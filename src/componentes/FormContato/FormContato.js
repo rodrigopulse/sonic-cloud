d7c831262c0a87463ca1e8477907b08a949ef44c
Rodrigo Godoy <contato@rodrigogodoy.net>
2019-12-18 21:17:09 -0300

    enviando email pelo phpmailer

--- src/componentes/FormContato/FormContato.js
+++ src/componentes/FormContato/FormContato.js
@@ -5,47 +5,50 @@ import './FormContato.scss';
 import '../../assets/sass/botoes.scss';
 
 class FormContato extends React.Component {
+	constructor(props){
+		super(props)
+	}
 	state = {
-    nome: '',
-    email: '',
-    telefone: '',
-    mensagem: '',
+		nome: '',
+		telefone: '',
+		email: '',
+		mensagem: '',
+		showModal: false
 	}
-
-	handleSubmit(e) {
-		e.preventDefault()
-		const{email, nome, telefone, mensagem} = this.state;
-		axios.post('https://cors-anywhere.herokuapp.com/https://vagalumeria.com.br/sendmail',
-		{
-			emailSend: email,
-			nome: nome,
-			telefone: telefone,
-			mensagem: mensagem
-		})
-		.then(function(){this.resetForm()})
-		.catch(function(error){console.log('erro: ', error)});
-	}
-	resetForm() {
-    this.setState({
-      nome: '',
-      email: '',
-      telefone: '',
-      mensagem: '',
-		})
-		alert("Mensagem enviada com sucesso");
-  }
-	handleChange = (param, e) => {
+  handleSubmit(e) {
+    e.preventDefault();
+    const { nome, telefone, email, mensagem } = this.state
+    const data = new FormData ()
+    data.append ('nome', nome)
+    data.append ('telefone', telefone)
+    data.append ('email', email)
+		data.append ('mensagem', mensagem)
+    axios.post('https://cors-anywhere.herokuapp.com/https://vagalumeria.com.br/sendmail/index.php', data)
+    .then( (response) => {
+			this.setState({
+				nome: '',
+				telefone: '',
+				email: '',
+				mensagem: '',
+				showModal: true
+			});
+    })
+    .catch( (response) => {
+      console.log(response);
+    });
+	};
+  handleChange = (param, e) => {
     this.setState({ [param]: e.target.value })
-  }
+	}
 	render() {
 		return(
 			<div className='formulario-contato'>
 				<form  onSubmit={this.handleSubmit.bind(this)}>
 						<div className="formulario-contato__grupo" id="formContato">
 							<input
-								name="nome"
+								id="nome"
 								value={this.state.nome}
-								onChange={this.handleChange.bind(this, 'nome')}
+								onChange={e => this.setState({ nome: e.target.value })}
 								className="formulario-contato__input"
 								placeholder="Digite seu nome"
 								required
@@ -53,9 +56,9 @@ class FormContato extends React.Component {
 						</div>
 						<div className="formulario-contato__grupo">
 							<input
-								name="email"
+								id="email"
 								value={this.state.email}
-								onChange={this.handleChange.bind(this, 'email')}
+								onChange={e => this.setState({ email: e.target.value })}
 								className="formulario-contato__input"
 								placeholder="Digite seu E-mail"
 								required
@@ -63,18 +66,18 @@ class FormContato extends React.Component {
 						</div>
 						<div className="formulario-contato__grupo">
 							<input
-								name="telefone"
+								id="telefone"
 								value={this.state.telefone}
-								onChange={this.handleChange.bind(this, 'telefone')}
+								onChange={e => this.setState({ telefone: e.target.value })}
 								className="formulario-contato__input"
 								placeholder="Digite seu Telefone"
 							/>
 						</div>
 						<div className="formulario-contato__grupo">
 							<textarea
-								name="mensagem"
+								id="mensagem"
 								value={this.state.mensagem}
-								onChange={this.handleChange.bind(this, 'mensagem')}
+								onChange={e => this.setState({ mensagem: e.target.value })}
 								className="formulario-contato__textarea"
 								placeholder="Escreva sua mensagem"
 								required
@@ -83,6 +86,14 @@ class FormContato extends React.Component {
 						</div>
 						<input className="botao-padrao" type="submit" value="Enviar Mensagem" />
 				</form>
+				{ this.state.showModal &&
+					<div className="modal">
+						<div className="modal__mensagem">
+							<h3>Mensagem enviada com sucesso!</h3>
+							<button className="botao-padrao modal__botao" onClick={e => this.setState({ showModal: false })}>Ok</button>
+						</div>
+					</div>
+				}
 			</div>
 		);
 	}
